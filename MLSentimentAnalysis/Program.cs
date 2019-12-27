09ce21648721af83cd04610e0dc2c52d987f0392
siawokoy <similola-a@hotmail.com>
2019-12-27 12:25:18 +0000

    first commit

---
+++ MLSentimentAnalysis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MLSentimentAnalysisML.Model;
+
+namespace MLSentimentAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to your Review Sentiment Analysis!");
+            Console.WriteLine("******");
+            Console.Write("Please enter your review:");
+            // Add input data
+            var input = new ModelInput();
+            input.Comment = Console.ReadLine();
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            if (result.Prediction.Equals(true))
+            {
+                Console.WriteLine("Your review sounds negative. Apologies, we will work on making things better for next time!");
+            }
+            else {
+                Console.WriteLine("Your review sounds positive. Glad you enjoyed it!");
+            }
+
+
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,8 @@
+ML Sentiment Analysis
+
+A simple C# program that uses a trained ML Model using ML.NET Model Builder to predict sentiment of reviews.
+
+Instructions.
+Clone respository on your local drive.
+Run program.cs in Visual Studio.
+Enter your review and click enter.
